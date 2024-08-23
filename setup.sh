1bb5c8deee9b721644481841ef555abe1277ba66
TheDeveloperEver <76601635+thedeveloperever@users.noreply.github.com>
2024-08-22 22:52:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo pacman -S rofi dex feh
 sudo pacman -S polybar i3 picom fish fastfetch kitty rofi
 sudo pacman -S python python-pip
-sudo pacmac -S ttf-jetbrains-mono-nerd ttf-font-awesome
+sudo pacman -S ttf-jetbrains-mono-nerd ttf-font-awesome
 pip install pywal
 
 echo "feh --bg-scale ~/Pictures/wallpapers/winter.png" > ~/.fehbg

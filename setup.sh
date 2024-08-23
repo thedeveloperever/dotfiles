660a40bc71e9f67581bc9a15121e84ea9ea79632
TheDeveloperEver <76601635+thedeveloperever@users.noreply.github.com>
2024-08-22 22:35:17 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo pacman -S rofi dex feh
 sudo pacman -S polybar i3 picom fish fastfetch kitty rofi
 sudo pacman -S python python-pip
 pip install pywal
-echo "feh --bg-scale /home/vawmpira/Pictures/winter.png" > ~/.fehbg
+echo "feh --bg-scale ~/Pictures/wallpapers/winter.png" > ~/.fehbg
 chmod +x .fehbg
 
 cp -r config/* ~/.config
